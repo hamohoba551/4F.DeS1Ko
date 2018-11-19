38a350a2f9ec38d7142b0d041bcbe1366e107fd7
hamohoba551 <43608567+hamohoba551@users.noreply.github.com>
2018-11-19 16:32:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,17 @@
+const Discord = require("discord.js");
 
+const client = new Discord.Client();
+
+client.on('ready', () => {
+
+    client.user.setGame(`الضربة الاولي ستؤلمك وتجعلك تصرخ.`,'https://www.twitch.tv/MeeRcY')
+
+});
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
