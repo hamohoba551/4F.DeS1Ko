b56ae85240da2bbff2a97a6753c1b39e3bfa3ee9
hamohoba551 <43608567+hamohoba551@users.noreply.github.com>
2018-11-19 20:47:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
 
-    client.user.setGame(`الضربة الاولي ستؤلمك وتجعلك تصرخ.`,'https://www.twitch.tv/MeeRcY')
+    client.user.setGame(`ضربتي الاولي ستؤلمك وتجعلك تصرخ.`,'https://www.twitch.tv/MeeRcY')
 
 });
 
